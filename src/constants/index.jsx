4cc93b91ea90569139afbd924a7c5716df694bec
alok-more <alokmore1903@gmail.com>
2025-03-30 12:38:20 +0530

    modified minor changes done

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,22 +1,22 @@
 import { SocialMediaProfiles } from "@/components/SocialMedia";
 
 export const navigation = [
-  {
-    title: "Work",
-    links: [
-      { title: "Amazon Clone", href: "/work/amazonclone" },
-      { title: "Bazar e-commerce", href: "/work/bazar" },
-      { title: "Blog 101", href: "/work/blog101" },
-      {
-        title: (
-          <>
-            See all <span aria-hidden="true">&rarr;</span>
-          </>
-        ),
-        href: "/work",
-      },
-    ],
-  },
+  // {
+  //   title: "Work",
+  //   links: [
+  //     { title: "Amazon Clone", href: "/work/amazonclone" },
+  //     { title: "Bazar e-commerce", href: "/work/bazar" },
+  //     { title: "Blog 101", href: "/work/blog101" },
+  //     {
+  //       title: (
+  //         <>
+  //           See all <span aria-hidden="true">&rarr;</span>
+  //         </>
+  //       ),
+  //       href: "/work",
+  //     },
+  //   ],
+  // },
   {
     title: "Company",
     links: [
